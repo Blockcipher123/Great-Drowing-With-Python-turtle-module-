5abca12034345f9834fce5dde194be6da88e03d1
Blockcipher123 <blockcipher0@gmail.com>
2022-05-28 17:37:28 +0530

    first commit

---
+++ Great-Drowing(With turtle module).py
@@ -0,0 +1,17 @@
+# Coded By Block_Cipher
+
+from turtle import *
+
+bgcolor('black')
+speed(-20)
+hideturtle()
+
+for i in range(120):
+	color('yellow')
+	circle(1)
+	color('orange')
+	circle(50)
+	right(3)
+	forward(3)
+
+done()
